fb853ee2fa79300ecf95bb367a1331460918452d
r1nple <rinatalbertovich24@gmail.com>
2026-05-29 14:12:02 +0300

    Добавлены классы-модели

---
+++ src/main/java/models/User.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private String name;
+    private String email;
+    private String password;
+
+}
